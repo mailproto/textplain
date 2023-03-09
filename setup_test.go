eea21ced3f03d2f6028365a40d93bc8a8647fad5
Stephen Young <stephen@customer.io>
2023-03-09 17:34:43 -0500

    add a couple benchmarks

--- setup_test.go
+++ setup_test.go
@@ -32,3 +32,42 @@ func runTestCase(t *testing.T, tc testCase) {
 	}
 
 }
+
+const html = `<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd"><html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <style type="text/css">
+      .button { color: red; }
+    </style>
+  </head>
+  <body>
+    <!-- start email -->
+    <h6>Small header</h6>
+
+<p style="font-weight: bold; color: red;" class="button">Hello></p>
+<a href="http://example.com"><img alt="An Example" src="https://example.com/image.jpg/></a><br/>
+<a href="https://example.com/fancy" class="fancy">Fancy text</a>
+</p>
+<ol>
+
+  <li>item one</li>
+
+  <li>item two</li>
+  <li>item three</li>
+
+</ol>
+
+<img src="https://example.com/footer-animation.gif" /></body></html>`
+
+func BenchmarkRegexp(b *testing.B) {
+	converter := textplain.NewRegexpConverter()
+	for i := 0; i < b.N; i++ {
+		_, _ = converter.Convert(html, textplain.DefaultLineLength)
+	}
+}
+
+func BenchmarkTree(b *testing.B) {
+	converter := textplain.NewTreeConverter()
+	for i := 0; i < b.N; i++ {
+		_, _ = converter.Convert(html, textplain.DefaultLineLength)
+	}
+}
