b3b3e98ae683186ed9d9778084d1d92d657a21da
Stephen Young <stephen@customer.io>
2026-09-20 22:35:30 -0400

    Give definition list terms and definitions their own lines

--- textplain_test.go
+++ textplain_test.go
@@ -642,3 +642,23 @@ func TestTables(t *testing.T) {
 		},
 	)
 }
+
+func TestDefinitionLists(t *testing.T) {
+	runTestCases(t,
+		testCase{
+			name:   "terms and definitions each get a line",
+			body:   `<dl><dt>Term</dt><dd>Definition</dd><dt>T2</dt><dd>D2</dd></dl>`,
+			expect: "Term\nDefinition\nT2\nD2",
+		},
+		testCase{
+			name:   "a term may have several definitions",
+			body:   `<dl><dt>Term</dt><dd>One</dd><dd>Two</dd></dl>`,
+			expect: "Term\nOne\nTwo",
+		},
+		testCase{
+			name:   "definitions keep their own markup",
+			body:   `<dl><dt>Term</dt><dd><a href="http://example.com">link</a></dd></dl>`,
+			expect: "Term\nlink ( http://example.com )",
+		},
+	)
+}
